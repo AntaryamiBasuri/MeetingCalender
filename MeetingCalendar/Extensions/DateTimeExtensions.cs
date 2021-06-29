a74441bb1935a05a231d451e487cd5e89ea800b0
AVisionaryDeveloper <avisionarydeveloper@gmail.com>
2021-06-29 19:14:31 +0530

    New methods added to the Meeting Calendar

--- MeetingCalendar/Extensions/DateTimeExtensions.cs
+++ MeetingCalendar/Extensions/DateTimeExtensions.cs
@@ -21,7 +21,7 @@ namespace MeetingCalendar.Extensions
 		/// <param name="dateTime">The date time instance.</param>
 		/// <returns>A new calibrated date time instance.</returns>
 		internal static DateTime CalibrateToMinutes(this DateTime dateTime)
-			=> new(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, 0, dateTime.Kind);
+			=> new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, 0, dateTime.Kind);
 
 		/// <summary>
 		/// Validates a date time against Min and Max value of DateTi.me.
